c660d9ecd97d15d995527454af733e76dddc06a8
macs-massimopiazza <me@massimopiazza.com>
2023-08-23 17:18:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <h1 style="color: #9cff2b">🎡🥰Vivaaaaa Classe 97🥰🎡</h1>
+  <h3>yayaya</h3>
+  <p style="color: #fa1afa">
+    💕I vostri insegnanti e coordinatori: Luca, Simone, Giovanni, Massimox2 💕
+  </p>
+  <h3 style="color: #ff0f0f">🥰🤣😱🤧🎍🦖 Qualche parola 🥰🤣😱🤧🎍🦖</h3>
+  <ul>
+    <li v-for="(frase, index) in frasi" :key="index">
+      {{ frase.testo }}
+      <span style="font-style: italic"> -{{ frase.nome }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      frasi: [
+        { testo: "non dire sacco se non ce l'hai nel gatto", nome: "Massimo" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #a4fbe5;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #5d00ff;
+}
+
+li {
+  font-size: 20px;
+  margin-bottom: 5px;
+}
+
+h1,
+h3,
+p,
+ul {
+  margin-bottom: 20px;
+}
+
+.proj {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ src/components/Meme.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    name: "Meme",
+    data() {
+        return {
+            fraseGenerata: false,
+            randomNumber: '',
+            frasi2: [
+                'Lo vedete il giro?',
+                'Smettila',
+                'Se andiamo a capo ogni tanto nella vita',
+                'È una porcata che la metà basta',
+                'Devo comprare una tastiera nuova',
+                'Uffatruffa',
+                'Buoooooooongiornoo',
+                'Buonanotte al secchio',
+                'Ciaone',
+                'Dubbidomande??',
+
+            ]
+        }
+    },
+    methods: {
+        GiovaSays() {
+            this.randomNumber = Math.floor(Math.random() * this.frasi2.length);
+            this.fraseGenerata = true;
+            console.log(this.randomNumber)
+        }
+    }
+}
+</script>
+
+<template>
+<button @click="GiovaSays()">What does Giovanni say?</button>
+
+<div class="frase" v-if="this.fraseGenerata">
+ {{ this.frasi2[this.randomNumber] }}
+</div>
+</template>
+
+<style scoped>
+.frase {
+    margin-top: 20px;
+    border-radius: 25px;
+    padding: 25px;
+    background-color: bisque;
+    border: 1px solid black;
+}
+
+</style>
