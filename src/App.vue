dd1da7a5c78c99d22d7436f8c0b084ad5c41f509
sim-sal <simone.saladino93@gmail.com>
2023-08-24 16:57:18 +0200

    commit Simone

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
         { testo: "non dire sacco se non ce l'hai nel gatto", nome: "Massimo" },
         { testo: "io sono un gatto e non un sacco", nome: "Massimo" },
         { testo: "Ciao a tutti è stato un onore", nome: "Michele" },
+        { testo: "Sono un orco e questo è il mio fork-one", nome: "Shrek" },
       ],
     };
   },
